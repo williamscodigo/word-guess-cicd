db87e2987178a03ddc8b8f72de9f7963d5add2b3
williamscodigo <willliamscodigo@gmail.com>
2024-12-19 22:53:23 -0500

    to satisfy render deployment error

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,32 +1,21 @@
 import express from 'express';
-import path from 'path';
-//using 1 import bellow to satify render deployment error
-import { fileURLToPath } from 'url';
-
 import db from './config/connection.js';
 import routes from './routes/index.js';
-
-//using 2 lines bellow to satify render deployment error
-// Define __dirname
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 await db();
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+
+// Serve up static assets in production - changed from previous set up to satisfy render deployment error
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('../client/dist'));
+}
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
-
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/dist')));
-
-   app.get('*', (_req, res) => {
-    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-  });
-}
 
 app.listen(PORT, () => {
   console.log(`API server running on port ${PORT}!`);
